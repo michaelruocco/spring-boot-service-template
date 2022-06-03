0e68de29b48b4d8dd1f210177b7da52d0e9aedcb
Michael Ruocco <michael.ruocco@hotmail.com>
2022-06-03 09:58:28 +0100

    updates openapi document config after updating dependencies

--- app/spring-app/src/main/java/uk/co/mruoc/service/app/config/OpenApiConfig.java
+++ app/spring-app/src/main/java/uk/co/mruoc/service/app/config/OpenApiConfig.java
@@ -1,8 +1,5 @@
 package uk.co.mruoc.service.app.config;
 
-import org.springdoc.core.SpringDocConfigProperties;
-import org.springdoc.core.SpringDocConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("org.openapitools.configuration")
 public class OpenApiConfig {
 
-    @Bean
-    public SpringDocConfiguration springDocConfiguration() {
-        return new SpringDocConfiguration();
-    }
-
-    @Bean
-    public SpringDocConfigProperties springDocConfigProperties() {
-        return new SpringDocConfigProperties();
-    }
+    // intentionally blank
 }
